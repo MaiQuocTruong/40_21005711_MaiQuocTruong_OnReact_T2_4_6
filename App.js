da77438435ffe315761498b822b6fbdc58dca657
H82M19\Student <Student>
2024-10-20 14:34:56 +0700

    xong giao dien electronic, hien thi duoc tat ca cac san pham tren tung muc categories list

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/Screen_01';  
+import LoginScreen from './screens/Screen_01';  // Đường dẫn có thể thay đổi tùy vào vị trí file LoginScreen.js
+import ElectronicsScreen from './screens/Screen_02';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Screen01">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen 
-          name="Screen01" 
-          component={HomeScreen} 
+          name="Login" 
+          component={LoginScreen} 
           options={{ headerShown: false }} // Ẩn header cho màn hình đăng nhập
         />
-
+        <Stack.Screen 
+          name="Electronics" 
+          component={ElectronicsScreen} 
+          options={{ 
+            headerTitle: '',
+            headerBackTitleVisible: false, 
+            headerStyle: {
+              elevation: 0, 
+              shadowOpacity: 0, 
+              borderBottomWidth: 0, 
+            },
+            header: () => null,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
